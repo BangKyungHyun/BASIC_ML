9c42b37a138de8fec465cd36fd68579d38bb9336
BangKyungHyun <72430106+BangKyungHyun@users.noreply.github.com>
2023-05-12 06:46:38 +0900

    수정

--- pytorch/Practice_08_01_logistic_regression.py
+++ pytorch/Practice_08_01_logistic_regression.py
@@ -1,8 +1,20 @@
 import torch
 import torch.nn as nn
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+from sklearn.datasets import load_breast_cancer
+cancer = load_breast_cancer()
+
+print(cancer.DESCR)
+
+df = pd.DataFrame(cancer.data, columns=cancer, feature_names)
+df['class'] = cancer.target
+'''
 from matplotlib import pyplot as plt
 
 x = torch.sort(torch.randn(100) * 10)[0]
 
 print('x =', x )
+'''
