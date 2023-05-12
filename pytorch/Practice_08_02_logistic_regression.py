902e41f733292171cf332a923119fc191d3478ae
BangKyungHyun <bangkh21@naver.com>
2023-05-12 16:04:47 +0900

    수정

--- pytorch/Practice_08_02_logistic_regression.py
+++ pytorch/Practice_08_02_logistic_regression.py
@@ -49,9 +49,9 @@ import torch.optim as optim
 data = torch.from_numpy(df[cols].values).float()
 print('data.shape = ', data.shape)
 # data.shape =  torch.Size([569, 11])
+
 x = data[:, :-1]
 y = data[:, -1:]
-
 print('x.shape, y.shape =',x.shape, y.shape)
 # x.shape, y.shape = torch.Size([569, 10]) torch.Size([569, 1])
 
@@ -96,13 +96,36 @@ for i in range(n_epochs):
     if (i + 1) % print_interval == 0:
         print('Epoch %d: loss=%.4e' % (i + 1, loss))
 
+'''
+Epoch 10000: loss=2.7718e-01
+Epoch 20000: loss=2.2865e-01
+Epoch 30000: loss=1.9965e-01
+Epoch 40000: loss=1.8072e-01
+Epoch 50000: loss=1.6749e-01
+Epoch 60000: loss=1.5775e-01
+Epoch 70000: loss=1.5028e-01
+Epoch 80000: loss=1.4436e-01
+Epoch 90000: loss=1.3956e-01
+Epoch 100000: loss=1.3558e-01
+Epoch 110000: loss=1.3222e-01
+Epoch 120000: loss=1.2935e-01
+Epoch 130000: loss=1.2686e-01
+Epoch 140000: loss=1.2469e-01
+Epoch 150000: loss=1.2276e-01
+Epoch 160000: loss=1.2105e-01
+Epoch 170000: loss=1.1952e-01
+Epoch 180000: loss=1.1813e-01
+Epoch 190000: loss=1.1688e-01
+Epoch 200000: loss=1.1573e-01
+'''
 # Let's see the result!
 correct_cnt = (y == (y_hat > .5)).sum()
 total_cnt = float(y.size(0))
 
 print('Accuracy: %.4f' % (correct_cnt / total_cnt))
+# Accuracy: 0.9666
 
 df= pd.DataFrame(torch.cat([y,y_hat], dim=1).detach().numpy(),
                  columns=["y","y_hat"])
-sns.hisplot(df, x='y_hat', hue='y', bins=50, stat='probability')
+sns.histplot(df, x='y_hat', hue='y', bins=50, stat='probability')
 plt.show()
