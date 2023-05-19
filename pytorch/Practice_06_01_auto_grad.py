c37ab7f48a9f18b23c014f1503d9a03c3086c38f
BangKyungHyun <bangkh21@naver.com>
2023-05-19 14:39:18 +0900

    수정

--- pytorch/Practice_06_01_auto_grad.py
+++ pytorch/Practice_06_01_auto_grad.py
@@ -1,5 +1,6 @@
 import torch
-
+# autograd 활성화 시키기 - requires_grad_(True)
+# Tensor를 생성하기 위해 사용하는 함수들의 파라미터로 requires_grad=True 를 넘김
 x = torch.FloatTensor([[1,2],[3,4]]).requires_grad_(True)
 
 x1 = x + 2
@@ -21,10 +22,15 @@ print("x3 =", x3)
 print("y =", y)
 # y = tensor(14., grad_fn=<SumBackward0>)
 
+# 역전파 시키기
 y.backward
+print('y.backward = ', y.backward)
+# y.backward =  <bound method Tensor.backward of tensor(14., grad_fn=<SumBackward0>)>
 
+# x 텐서의 기울기가 None 임
 print("x.grad = ",x.grad)
 # x.grad =  None
+
 print("x = ",x)
 # x =  tensor([[1., 2.],
 #        [3., 4.]], requires_grad=True)
@@ -34,8 +40,10 @@ print("x = ",x)
 # Traceback (most recent call last):
 #  File "C:\Users\KBDS\PycharmProjects\BASIC_ML\pytorch\Practice_06_01_auto_grad.py", line 32, in <module>
 #    print("x3.numpy() = ",x3.numpy())
-#RuntimeError: Can't call numpy() on Tensor that requires grad. Use tensor.detach().numpy() instead.
+# RuntimeError: Can't call numpy() on Tensor that requires grad. Use tensor.detach().numpy() instead.
+# Grad가 필요한 Tensor에서 numpy()를 호출할 수 없습니다. 대신 tensor.detach().numpy()를 사용하세요.
 
+# 텐서는 .numpy() 메서드(method)를 호출하여 NumPy 배열로 변환할 수 있습니다.
 print("x3.detach_().numpy() = ",x3.detach_().numpy())
 # x3.detach_().numpy() =  [[-3.  0.]
 #  [ 5. 12.]]
