7e08cf5eb1f261165048ede821dec344a94fdcdb
BangKyungHyun <bangkh21@naver.com>
2023-05-22 17:52:16 +0900

    수정

--- pytorch/Practice_06_02_gradient_descent.py
+++ pytorch/Practice_06_02_gradient_descent.py
@@ -24,22 +24,23 @@ loss = F.mse_loss(x, target)
 print("loss =", loss)
 # loss = tensor(0.1928, grad_fn=<MseLossBackward0>)
 
-threshold = 1e-10   # 임계치
+threshold = 1e-11   # 임계치
 learning_rate = 1.
 iter_cnt = 0
 # while 반복문을 사용하여 두 텐서 값의 차이가 threshold의 값보다 작아질 때 까지
 # 미분 및 경사하강법을 반복 수행
 
 while loss > threshold:
+
     iter_cnt += 1
     # backward 함수를 통해 편미분을 수행한다는 것인데 편미분을 통해 얻어진 기울기들이
     # x.grad에 자동 저장되고 이 값을 활용하여 경사하강법을 수행한다.
     # backward를 호출하기 위한 텐서의 크기는 스칼라이어야 함
     # 만약 스칼라가 아닌 경우에 backward를 호출하면 파이토치는 오류를 발생시킴
 
- -   # 기울기 계산
+    # 기울기 계산
     loss.backward()
-    x = x - learning_rate * x.grad
+    x = x - learning_rate * x.grad  # 경사 기울기 값이 x.grad에 자동 저장
 
     # detach(분리하다) - 기존 Tensor에서 gradient 전파가 안되는 텐서 생성
     x.detach_() # .
