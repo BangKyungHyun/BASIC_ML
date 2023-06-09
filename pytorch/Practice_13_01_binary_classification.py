ae97797acc2697b8ef478c6fba0df9c5a8d03143
BangKyungHyun <bangkh21@naver.com>
2023-06-09 17:57:48 +0900

    수정

--- pytorch/Practice_13_01_binary_classification.py
+++ pytorch/Practice_13_01_binary_classification.py
@@ -1,5 +1,5 @@
-# 로지스틱 회귀logistic regression에서 실습했던 유방암 예측 데이터셋을 활용하여 딥러닝을 통한 이진 분류를 실습하도록 합니다.
-# 다음과 같이 필요한 라이브러리들을 불러옵니다
+# 로지스틱 회귀logistic regression에서 실습했던 유방암 예측 데이터셋을 활용하여 딥러닝을 통한 이진 분류를 실습하도록 함
+# 다음과 같이 필요한 라이브러리들을 불러옴
 
 import numpy as np
 import pandas as pd
@@ -10,20 +10,21 @@ from copy import deepcopy
 from sklearn.preprocessing import StandardScaler
 from sklearn.datasets import load_breast_cancer
 
-# 유방암 데이터셋을 판다스Pandas 데이터프레임에 넣고, class 열에 정답 데이터를 넣어줍니다.
+# 유방암 데이터셋을 판다스Pandas 데이터프레임에 넣고, class 열에 정답 데이터를 넣어 줌
 
 cancer = load_breast_cancer()
 df = pd.DataFrame(cancer.data, columns=cancer.feature_names)
 df['class'] = cancer.target
 
-# 앞서 로지스틱 회귀에서 데이터셋에 대한 분석은 어느정도 진행하였으니, 바로 데이터셋 분할에 나서도록 하겠습니다. 이를 위해서 파이토치에서 필요한 패키지들을 불러옵니다.
+# 앞서 로지스틱 회귀에서 데이터셋에 대한 분석은 어느정도 진행하였으니, 바로 데이터셋 분할에 나서도록 하겠음.
+# 이를 위해서 파이토치에서 필요한 패키지들을 불러옴
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-# 데이터프레임의 넘파이Numpy 데이터를 파이토치 데이터로 변환하고 크기를 확인합니다.
+# 데이터프레임의 넘파이 데이터를 파이토치 데이터로 변환하고 크기를 확인함
 
 data = torch.from_numpy(df.values).float()
 
@@ -31,10 +32,12 @@ x = data[:, :-1]
 y = data[:, -1:]
 
 print('x.shape, y.shape = ', x.shape, y.shape)
+# x.shape, y.shape =  torch.Size([569, 30]) torch.Size([569, 1])
 
-# 유방암 데이터는 569개의 입출력 쌍으로 샘플이 구성되어 있습니다. 입력 데이터는 한 샘플이 569 차원의 벡터로 구성되며, 출력 데이터 샘플은 하나의 이진 값으로 표현됩니다.
+# 유방암 데이터는 569개의 입출력 쌍으로 샘플이 구성되어 있음.
+# 입력 데이터는 한 샘플이 569 차원의 벡터로 구성되며, 출력 데이터 샘플은 하나의 이진 값으로 표현됨
 
-# 이제 이 데이터를 6:2:2의 비율로 학습/검증/테스트 데이터셋으로 나눠보겠습니다. 앞서 챕터의 실습과 똑같은 코드입니다.
+# 이제 이 데이터를 6:2:2의 비율로 학습/검증/테스트 데이터셋으로 나눠보겠음. 앞서 챕터의 실습과 똑같은 코드임
 
 # Train / Valid / Test ratio
 ratios = [.6, .2, .2]
@@ -45,9 +48,10 @@ test_cnt = data.size(0) - train_cnt - valid_cnt
 cnts = [train_cnt, valid_cnt, test_cnt]
 
 print("Train %d / Valid %d / Test %d samples." % (train_cnt, valid_cnt, test_cnt))
-# 6:2:2의 비율로 나눌 경우, 각각 341개, 113개, 115개의 샘플로 학습/검증/테스트 데이터셋이 구성됩니다.
+# Train 341 / Valid 113 / Test 115 samples.
+# 6:2:2의 비율로 나눌 경우, 각각 341개, 113개, 115개의 샘플로 학습/검증/테스트 데이터셋이 구성됨
 
-# 이제 이 갯수대로 랜덤하게 섞어 나누어줍니다.
+# 이제 이 갯수대로 랜덤하게 섞어 나누어 줌
 
 indices = torch.randperm(data.size(0))
 
@@ -59,8 +63,10 @@ y = y.split(cnts, dim=0)
 
 for x_i, y_i in zip(x, y):
     print(x_i.size(), y_i.size())
-
-# 학습 데이터를 기준으로 표준 스케일러를 학습한 후, 해당 스케일러를 학습/검증/테스트 데이터셋에 똑같이 적용합니다.
+# torch.Size([341, 30]) torch.Size([341, 1])
+# torch.Size([113, 30]) torch.Size([113, 1])
+# torch.Size([115, 30]) torch.Size([115, 1])
+# 학습 데이터를 기준으로 표준 스케일러를 학습한 후, 해당 스케일러를 학습/검증/테스트 데이터셋에 똑같이 적용함
 
 scaler = StandardScaler()
 scaler.fit(x[0].numpy())
@@ -70,10 +76,10 @@ x = [torch.from_numpy(scaler.transform(x[0].numpy())).float(),
      torch.from_numpy(scaler.transform(x[2].numpy())).float()]
 
 # 학습 코드 구현
-# nn.Sequential 클래스를 활용하여 모델을 구현합니다. 선형 계층과 리키 렐루를 차례로 집어넣어줍니다.[1]
-# 이어지는 계층의 입력 크기가 이전 계층의 출력 크기와 맞도록 해줘야 함을 참고하세요.
-# 모델 구조의 마지막에는 시그모이드를 넣어주어 이진 분류를 위한 준비를 마칩니다.
-# 그리고 아담 옵티마이저에 선언한 모델 가중치 파라미터를 등록해줍니다.
+# nn.Sequential 클래스를 활용하여 모델을 구현. 선형 계층과 리키 렐루를 차례로 집어넣어줌
+# 이어지는 계층의 입력 크기가 이전 계층의 출력 크기와 맞도록 해줘야 함을 참고 필요
+# 모델 구조의 마지막에는 시그모이드를 넣어주어 이진 분류를 위한 준비를 마침
+# 그리고 아담 옵티마이저에 선언한 모델 가중치 파라미터를 등록해 줌
 
 model = nn.Sequential(
     nn.Linear(x[0].size(-1), 250),
@@ -92,7 +98,7 @@ model = nn.Sequential(
 
 optimizer = optim.Adam(model.parameters())
 
-# 학습에 필요한 하이퍼파라미터를 설정해줍니다. 어차피 조기 종료를 걸어두었으니, n_epochs 파라미터를 굉장히 크게 잡아보았습니다.
+# 학습에 필요한 하이퍼파라미터를 설정해 줌. 어차피 조기 종료를 걸어두었으니, n_epochs 파라미터를 굉장히 크게 잡아 보았음
 
 n_epochs = 10000
 batch_size = 32
@@ -103,12 +109,11 @@ lowest_loss = np.inf
 best_model = None
 lowest_epoch = np.inf
 
-# 다음은 모델 학습 이터레이션을 진행하는 반복문 코드입니다.
+###############################################################################
+# 다음은 모델 학습 이터레이션을 진행하는 반복문 코드임
 # 여기서 눈여겨 보아야 할 부분은 손실 함수를 BCE 함수로 쓴다는 점이고,
-# 이는 F.binary_cross_entropy를 활용하는 것을 볼 수 있습니다.
-# 나머지는 앞서 챕터에서 실습한 코드와 거의 똑같기 때문에 자세한 설명은 생략하도록 하겠습니다.
-# 설명이 필요하신 독자분들은 앞 챕터들을 참고 바랍니다.
-# 이처럼 한번 만든 학습 코드는 제대로 만든다면 계속 재활용해서 쓸 수 있게 됩니다.
+# 이는 F.binary_cross_entropy를 활용하는 것을 볼 수 있음
+###############################################################################
 
 train_history, valid_history = [], []
 
@@ -154,6 +159,7 @@ for i in range(n_epochs):
     train_history += [train_loss]
     valid_history += [valid_loss]
 
+
     if (i + 1) % print_interval == 0:
         print('Epoch %d: train loss=%.4e  valid_loss=%.4e  lowest_loss=%.4e' % (
             i + 1,
@@ -173,22 +179,20 @@ for i in range(n_epochs):
             break
 
 print("The best validation loss from epoch %d: %.4e" % (lowest_epoch + 1, lowest_loss))
+# The best validation loss from epoch 6: 5.2692e-02
 model.load_state_dict(best_model)
 
-# 학습을 실행하면 다음과 같이 10 에포크마다 손실 값을 프린트하는 것을 확인할 수 있습니다.
-# 재미있게도 워낙 데이터가 작고 단순하기 때문인지 16번째 에포크에서 최소 손실 값을 기록한 것을 확인할 수 있습니다.
+# 학습을 실행하면 다음과 같이 10 에포크마다 손실 값을 프린트하는 것을 확인할 수 있음
+# 재미있게도 워낙 데이터가 작고 단순하기 때문인지 16번째 에포크에서 최소 손실 값을 기록한 것을 확인할 수 있음
 
 ################################################################################
 # 이후에는 확연한 오버피팅이 시작되어 학습 손실 값은 계속해서 작아지는데 반해,
-# 검증 손실 값이 계속해서 올라가버리는 것을 볼 수 있습니다.
+# 검증 손실 값이 계속해서 올라가 버리는 것을 볼 수 있습니다.
 ################################################################################
 
-
-# [1]: 사실 데이터가 적기 때문에 이렇게 깊게 쌓을 필요가 없을 수도 있습니다.
-
 # 손실 곡선 확인
-# 그래서 손실 곡선을 찍어 확인해보면 학습 초반부 함께 내려가던 손실 값이 16번째 에포크 이후로 벌어지는 것을 확인할 수 있습니다.
-# 실제로 조기 종료 파라미터를 바꿔 학습을 더 진행해보면 학습 손실 값은 계속해서 내려가는 것을 확인할 수 있습니다.
+# 그래서 손실 곡선을 찍어 확인해보면 학습 초반부 함께 내려가던 손실 값이 16번째 에포크 이후로 벌어지는 것을 확인할 수 있음
+# 실제로 조기 종료 파라미터를 바꿔 학습을 더 진행해 보면 학습 손실 값은 계속해서 내려가는 것을 확인할 수 있음
 
 plot_from = 2
 
@@ -203,7 +207,7 @@ plt.yscale('log')
 plt.show()
 
 # 결과 확인
-# 앞서와 마찬가지로 같은 코드를 활용하여 테스트 데이터셋에 대해서 평균 손실 값을 구해봅니다.
+# 앞서와 마찬가지로 같은 코드를 활용하여 테스트 데이터셋에 대해서 평균 손실 값을 구해 봄
 
 test_loss = 0
 y_hat = []
@@ -230,19 +234,24 @@ print("Train loss: %.4e" % sorted_history[0][0])
 print("Valid loss: %.4e" % sorted_history[0][1])
 print("Test loss: %.4e" % test_loss)
 
-# 이번 실습은 일반적인 회귀regression가 아닌 분류 실습이기 때문에, 정확도accruracy도 계산할 수 있습니다.
-# 모델의 예측 값이 0.5보다 클 경우에는 1로 예측한 것으로 가정하고, 작거나 같은 경우에는 0으로 예측한 것이라고 가정합니다.
-# 그렇게 가정했을 때, 실제 정답과 똑같은 갯수를 구하고 전체 갯수로 나누면 정확도를 구할 수 있습니다.
+# Train loss: 1.9961e-02
+# Valid loss: 5.2692e-02
+# Test loss: 1.7916e-01
+
+# 이번 실습은 일반적인 회귀가 아닌 분류 실습이기 때문에, 정확도도 계산할 수 있음
+# 모델의 예측 값이 0.5보다 클 경우에는 1로 예측한 것으로 가정하고, 작거나 같은 경우에는 0으로 예측한 것이라고 가정함
+# 그렇게 가정했을 때, 실제 정답과 똑같은 갯수를 구하고 전체 갯수로 나누면 정확도를 구할 수 있음
 
 correct_cnt = (y[2] == (y_hat > .5)).sum()
 total_cnt = float(y[2].size(0))
 
 print('Test Accuracy: %.4f' % (correct_cnt / total_cnt))
+# Test Accuracy: 0.9652
 
-# 계산 결과 테스트 데이터셋에 대해서 무려 98%의 높은 정확도를 보이는 것으로 나타났습니다.
+# 계산 결과 테스트 데이터셋에 대해서 무려 98%의 높은 정확도를 보이는 것으로 나타남
 
-# 그럼 예측 값의 분포도 확인해보도록 하겠습니다. 각 클래스별로 다른 색깔로 칠해
-# 히스토그램histogram으로 나타냈을 떄, 각 클래스의 분포가 서로 겹치지 않을수록 좋은 예측이라고 볼 수 있습니다.
+# 그럼 예측 값의 분포도 확인해보도록 하겠음. 각 클래스별로 다른 색깔로 칠해
+# 히스토그램으로 나타냈을 떄, 각 클래스의 분포가 서로 겹치지 않을수록 좋은 예측이라고 볼 수 있음.
 
 df = pd.DataFrame(torch.cat([y[2], y_hat], dim=1).detach().numpy(),
                   columns=["y", "y_hat"])
@@ -250,13 +259,15 @@ df = pd.DataFrame(torch.cat([y[2], y_hat], dim=1).detach().numpy(),
 sns.histplot(df, x='y_hat', hue='y', bins=50, stat='probability')
 plt.show()
 
-# 그림을 확인해보면 극히 일부를 제외하고 대부분 왼쪽과 오른쪽에 잘 나뉘어져 있는 것을 확인할 수 있습니다
+# 그림을 확인해보면 극히 일부를 제외하고 대부분 왼쪽과 오른쪽에 잘 나뉘어져 있는 것을 확인할 수 있음
 
-# 이번에는 AUROC를 구해보도록 하겠습니다. AUROC는 sklearn을 활용해서 쉽게 계산할 수 있습니다.
+# 이번에는 AUROC를 구해보도록 하겠음. AUROC는 sklearn을 활용해서 쉽게 계산할 수 있음
 
 from sklearn.metrics import roc_auc_score
 
 print('roc_auc_score = ',roc_auc_score(df.values[:, 0], df.values[:, 1]))
+# roc_auc_score =  0.984838497033619
 
-# 정답 값과 예측 실수 값을 함께 넣어주면 자동으로 계산해주는데요,
-# 앞서 그림에서 보았듯이 두 클래스의 분포가 확연하게 나뉠 수 있는 상황이기 때문에 매우 높은 값을 보여줍니다.
+# 정답 값과 예측 실수 값을 함께 넣어주면 자동으로 계산함,
+# 앞서 그림에서 보았듯이 두 클래스의 분포가 확연하게 나뉠 수 있는 상황이기 때문에 매우 높은 값을 보여줌
+
--- pytorch/Practice_14_01_regularizations.py
+++ pytorch/Practice_14_01_regularizations.py
@@ -27,15 +27,19 @@ y = train.targets
 
 x = x.view(x.size(0), -1)
 print(x.shape, y.shape)
+# torch.Size([60000, 784]) torch.Size([60000])
 
 input_size = x.size(-1)
 output_size = int(max(y)) + 1
 
 print('input_size: %d, output_size: %d' % (input_size, output_size))
+# input_size: 784, output_size: 10
 
-# 중요한 점은 이전 실습에서와 같이 2차원 이미지 행렬을 1차원 벡터로 변환했다는 것입니다. 따라서 학습 데이터의 입력과 출력 텐서 크기는 다음과 같습니다.
+# 중요한 점은 이전 실습에서와 같이 2차원 이미지 행렬을 1차원 벡터로 변환했다는 것임.
+# 따라서 학습 데이터의 입력과 출력 텐서 크기는 다음과 같음
 
-# MNIST는 테스트 데이터셋이 이미 정해져 있기 때문에, 학습 데이터셋을 학습과 검증 데이터셋으로 나누기만 하면 됩니다. 따라서 8:2의 비율로 나누도록 합니다.
+# MNIST는 테스트 데이터셋이 이미 정해져 있기 때문에,
+# 학습 데이터셋을 학습과 검증 데이터셋으로 나누기만 하면 됨. 따라서 8:2의 비율로 나누도록 함
 
 # Train / Valid ratio
 ratios = [.8, .2]
@@ -46,6 +50,7 @@ test_cnt = len(test.data)
 cnts = [train_cnt, valid_cnt]
 
 print("Train %d / Valid %d / Test %d samples." % (train_cnt, valid_cnt, test_cnt))
+# Train 48000 / Valid 12000 / Test 10000 samples.
 
 indices = torch.randperm(x.size(0))
 
@@ -61,26 +66,33 @@ y += [test.targets]
 for x_i, y_i in zip(x, y):
     print(x_i.size(), y_i.size())
 
-# 그럼 48,000장의 학습 샘플과 12,000장의 검증 샘플, 그리고 10,000장의 테스트 샘플로 데이터셋이 구성됩니다
+# torch.Size([48000, 784]) torch.Size([48000])
+# torch.Size([12000, 784]) torch.Size([12000])
+# torch.Size([10000, 784]) torch.Size([10000])
+
+# 그럼 48,000장의 학습 샘플과 12,000장의 검증 샘플, 그리고 10,000장의 테스트 샘플로 데이터셋이 구성됨
 
 # 학습 코드 구현
-# 앞서의 실습에서는 대부분 nn.Sequential에 원하는 계층과 활성함수를 집어넣어 쉽고 간편하게 모델을 구현할 수 있었습니다.
-# 이번에도 비슷하게 진행할 수 있지만, 한 단계 발전된 방향으로 나아가고자 합니다.
-# 사실 생각해보면 앞서의 실습들에서 활용된 모델들도 선형 계층linear layer과
-# 비선형 활성 함수non-linear activation function의 반복이었습니다.
+# 앞서의 실습에서는 대부분 nn.Sequential에 원하는 계층과 활성함수를 집어넣어
+# 쉽고 간편하게 모델을 구현할 수 있음
+# 이번에도 비슷하게 진행할 수 있지만, 한 단계 발전된 방향으로 나아가고자 함
+# 사실 생각해보면 앞서의 실습들에서 활용된 모델들도 선형 계층과
+# 비선형 활성 함수의 반복이었습니다.
 # 즉, 하나의 층이 선형 계층과 비선형 활성 함수의 조합으로 이루어지게 되고,
-# 이것을 전체 모듈에 대한 부분 모듈 또는 서브 모듈로 볼 수 있습니다.
-# 그럼 서브 모듈이 입출력 크기만 바뀌어서 반복되고 있던 것으로 볼 수 있습니다.
+# 이것을 전체 모듈에 대한 부분 모듈 또는 서브 모듈로 볼 수 있음
+# 그럼 서브 모듈이 입출력 크기만 바뀌어서 반복되고 있던 것으로 볼 수 있음
 # 이번에는 여기에 정규화 계층이 더해져 “선형 계층 + 비선형 활성 함수 + 정규화 계층”이 하나의 서브 모듈이 될 것이고,
-# 마찬가지로 입출력 크기만 바뀌어서 반복 사용되는 것입니다. 그럼 이것에 착안하여
-# 서브 모듈을 nn.Module을 상속받아 하나의 클래스로 정의하고, nn.Sequential에 “선형 계층 + 비선형 활성 함수 + 정규화 계층”을
-# 각각 인자로 넣어주는 대신에, 정의한 클래스 객체를 넣어주면 될 것입니다.
+# 마찬가지로 입출력 크기만 바뀌어서 반복 사용되는 것임.
+# 그럼 이것에 착안하여 서브 모듈을 nn.Module을 상속받아 하나의 클래스로 정의하고,
+# nn.Sequential에 “선형 계층 + 비선형 활성 함수 + 정규화 계층”을
+# 각각 인자로 넣어주는 대신에, 정의한 클래스 객체를 넣어주면 될 것임
 
-# 다음 코드는 서브 모듈 클래스를 정의하는 코드입니다. 이 모듈은 생성시에 배치 정규화batch normalization와
-# 드랍아웃dropout 중에서 선택받고, 입출력 크기를 입력받습니다.
-# 그래서 self.block에 선형 계층, 리키 렐루, 정규화 계층을 nn.Sequential에 넣어 가지고 있습니다.
-# 배치 정규화를 사용한 경우에는 앞서 사용된 선형 계층의 출력 크기를 넣어주어야 하고,
-# 드랍아웃의 경우에는 확률 값을 넣어주어야 합니다. 그리고 forward 함수에서 피드포워드를 구현해줍니다.
+# 다음 코드는 서브 모듈 클래스를 정의하는 코드임.
+# 이 모듈은 생성시에 배치 정규화와 드랍아웃 중에서 선택받고, 입출력 크기를 입력받음.
+# 그래서 self.block에 선형 계층, 리키 렐루, 정규화 계층을 nn.Sequential에 넣어 가지고 있음
+# 배치 정규화를 사용한 경우에는 앞서 사용된 선형 계층의 출력 크기를 넣어 주어야 하고,
+# 드랍아웃의 경우에는 확률 값을 넣어주어야 함.
+# 그리고 forward 함수에서 피드포워드를 구현해 줌
 
 class Block(nn.Module):
 
@@ -112,12 +124,13 @@ class Block(nn.Module):
 
         return y
 
-# 이렇게 정의된 Block 클래스를 곧이어 정의할 MyModel 클래스에서 활용할 계획입니다.
-# 다음의 코드는 MyModel 클래스를 정의한 코드로, nn.Module을 상속받아 만들었습니다.
-# 마찬가지로 init 함수에서 필요한 객체들을 미리 선언해주는데, self.layer는 nn.Sequential 객체를 가지고 있고,
-# 그 내부는 Block 클래스 객체들로 채워져 있습니다.
+# 이렇게 정의된 Block 클래스를 곧이어 정의할 MyModel 클래스에서 활용할 계획임
+# 다음의 코드는 MyModel 클래스를 정의한 코드로, nn.Module을 상속받아 만들었음
+# 마찬가지로 init 함수에서 필요한 객체들을 미리 선언해주는데,
+# self.layer는 nn.Sequential 객체를 가지고 있고,
+# 그 내부는 Block 클래스 객체들로 채워져 있음
 # 이전 챕터의 실습과 똑같이 784(input_size) -> 500 -> 400 -> 300 -> 200 -> 100 -> 50 -> 10(output_size)
-# 으로 이어지도록 구성되어 있고, 마지막에는 로그소프트맥스가 위치합니다.
+# 으로 이어지도록 구성되어 있고, 마지막에는 로그소프트맥스가 위치함
 
 class MyModel(nn.Module):
 
@@ -146,22 +159,23 @@ class MyModel(nn.Module):
 
         return y
 
-# MyModel 모델 객체를 선언하고 프린트해보도록 합니다.
+# MyModel 모델 객체를 선언하고 프린트 함
 
 model = MyModel(input_size,
                 output_size,
                 use_batch_norm=True)
 
-print(model)
+print('model = \n',model)
 
-# 모델을 프린트해보면 기존에 비해 조금 내용이 길어졌지만, 보기 좋게 서브 모듈들이 나뉘어져 반복되고 있는 것을 볼 수 있습니다.
-
-# 그리고 모델 가중치 파라미터를 아담Adam 옵티마이저에 등록하고, NLL 손실 함수를 선언합니다.
+# 모델을 프린트해 보면 기존에 비해 조금 내용이 길어졌지만,
+# 보기 좋게 서브 모듈들이 나뉘어져 반복되고 있는 것을 볼 수 있dma
+# 그리고 모델 가중치 파라미터를 아담 옵티마이저에 등록하고, NLL 손실 함수를 선언함
 
 optimizer = optim.Adam(model.parameters())
 crit = nn.NLLLoss()
 
-# CUDA가 활용 가능할 경우 GPU가 기본 디바이스가 되도록 device 변수에 집어넣어주고, 이를 활용하여 모델과 텐서를 원하는 디바이스로 이동 및 복사 합니다
+# CUDA가 활용 가능할 경우 GPU가 기본 디바이스가 되도록 device 변수에 집어 넣어주고,
+# 이를 활용하여 모델과 텐서를 원하는 디바이스로 이동 및 복사함
 
 device = torch.device('cpu')
 if torch.cuda.is_available():
@@ -172,7 +186,7 @@ model = model.to(device)
 x = [x_i.to(device) for x_i in x]
 y = [y_i.to(device) for y_i in y]
 
-# 학습에 필요한 하이퍼 파라미터 및 변수들을 미리 초기화 합니다.
+# 학습에 필요한 하이퍼 파라미터 및 변수들을 미리 초기화 함.
 
 n_epochs = 1000
 batch_size = 256
@@ -184,7 +198,7 @@ best_model = None
 early_stop = 50
 lowest_epoch = np.inf
 
-# 다음은 학습을 위한 코드입니다. 똑같은 코드가 계속해서 쓰이고 있습니다. 설명은 생략하도록 하겠습니다.
+# 다음은 학습을 위한 코드임. 똑같은 코드가 계속해서 쓰이고 있음.
 
 train_history, valid_history = [], []
 
@@ -252,17 +266,27 @@ for i in range(n_epochs):
             break
 
 print("The best validation loss from epoch %d: %.4e" % (lowest_epoch + 1, lowest_loss))
+# The best validation loss from epoch 55: 7.4972e-02
 model.load_state_dict(best_model)
-
-# 학습 코드를 실행하면 다음과 같은 학습 결과를 얻을 수 있습니다
+#
+# Epoch 10: train loss=2.7047e-02  valid_loss=8.2397e-02  lowest_loss=8.2002e-02
+# Epoch 20: train loss=1.1948e-02  valid_loss=7.9099e-02  lowest_loss=7.6479e-02
+# Epoch 30: train loss=1.1642e-02  valid_loss=8.9331e-02  lowest_loss=7.5532e-02
+# Epoch 40: train loss=6.8058e-03  valid_loss=8.4426e-02  lowest_loss=7.5532e-02
+# Epoch 50: train loss=5.4244e-03  valid_loss=8.2641e-02  lowest_loss=7.5532e-02
+# Epoch 60: train loss=6.2961e-03  valid_loss=7.9873e-02  lowest_loss=7.4972e-02
+# Epoch 70: train loss=3.2152e-03  valid_loss=7.7282e-02  lowest_loss=7.4972e-02
+# Epoch 80: train loss=9.2877e-04  valid_loss=8.2276e-02  lowest_loss=7.4972e-02
+# Epoch 90: train loss=1.6897e-03  valid_loss=8.6404e-02  lowest_loss=7.4972e-02
+# Epoch 100: train loss=2.8526e-03  valid_loss=9.2352e-02  lowest_loss=7.4972e-02
+# 학습 코드를 실행하면 다음과 같은 학습 결과를 얻을 수 있음
 
 # 16번째 에포크에서 최소 검증 손실 값을 얻었음을 볼 수 있고,
-# 학습 손실 값은 계속해서 낮아지며 오버피팅이 진행되고 있는 것을 확인할 수 있습니다.
+# 학습 손실 값은 계속해서 낮아지며 오버피팅이 진행되고 있는 것을 확인할 수 있음
 # 
-# 앞서 챕터에서 진행되었던 MNIST 분류에 비해 더 낮은 검증 손실 값을 얻은 것을 확인할 수 있습니다.
+# 앞서 챕터에서 진행되었던 MNIST 분류에 비해 더 낮은 검증 손실 값을 얻은 것을 확인할 수 있음
 
 # 손실 곡선 확인
-
 plot_from = 0
 
 plt.figure(figsize=(20, 10))
@@ -275,7 +299,8 @@ plt.plot(
 plt.yscale('log')
 plt.show()
 
-# 손실 곡선을 그려보면 앞서 프린트 된 내용과 같이 학습 손실 곡선은 계속해서 내려가는 것을 볼 수 있고, 검증 손실 곡선은 16 에포크 이후부터 천천히 올라가는 것을 볼 수 있습니다.
+# 손실 곡선을 그려보면 앞서 프린트 된 내용과 같이 학습 손실 곡선은 계속해서
+# 내려가는 것을 볼 수 있고, 검증 손실 곡선은 16 에포크 이후부터 천천히 올라가는 것을 볼 수 있음
 
 # 결과 확인
 # 과연 테스트셋에 대해서도 이전 챕터보다 더 좋은 성능이 나왔을까요?
@@ -300,33 +325,51 @@ test_loss = test_loss / len(x_)
 y_hat = torch.cat(y_hat, dim=0)
 
 print("Test Loss: %.4e" % test_loss)
+# Test Loss: 8.3356e-02
 
 correct_cnt = (y[-1].squeeze() == torch.argmax(y_hat, dim=-1)).sum()
 total_cnt = float(y[-1].size(0))
 
 print('Test Accuracy: %.4f' % (correct_cnt / total_cnt))
+# Test Accuracy: 0.9818
 
-# 이전 챕터의 실습 결과에 비해 훨씬 더 좋은 결과가 나오는 것을 볼 수 있습니다.
+# 이전 챕터의 실습 결과에 비해 훨씬 더 좋은 결과가 나오는 것을 볼 수 있음
 
-# 그럼 지난 챕터 대비 얼마나 성능이 좋아졌을까요? 단순히 성능의 개선폭을 보는 것은 좋은 방법이 아닐 수 있습니다.
-# 예를 들어 성능 60%의 모델이 70%로 개선되는 것보다, 97%의 모델이 98%로 개선되는 것이 더 좋은 것임을 알고 있습니다.
-# 이때 우리는 ERRError Reduction Rate을 통해 상대적인 모델의 개선 폭을 측정할 수 있습니다.
-# 지난 챕터 대비 이번 챕터에서 상승한 성능을 비교하면 다음과 같습니다.
+# 그럼 지난 챕터 대비 얼마나 성능이 좋아졌을까요? 단순히 성능의 개선폭을 보는 것은 좋은 방법이 아닐 수 있음
+# 예를 들어 성능 60%의 모델이 70%로 개선되는 것보다, 97%의 모델이 98%로 개선되는 것이 더 좋은 것임을 알고 있음
+# 이때 우리는 ERRError Reduction Rate을 통해 상대적인 모델의 개선 폭을 측정할 수 있음
+# 지난 챕터 대비 이번 챕터에서 상승한 성능을 비교하면 다음과 같음
 
-# 지난 챕터 대비 오류가 약 31% 줄어든 것을 볼 수 있습니다.
-# 물론 지금은 지난 챕터의 실습과 이번 챕터의 실습 모두 각 한 번 씩만 돌린 것이기 때문에 적절한 비교라고는 볼 수 없습니다.
-# 만약 제대로 비교하고자 한다면, 최소한 5번 이상 같은 실험을 반복하여 평균 테스트 정확도를 측정한 후, ERR을 계산해볼 수 있을 것입니다.
+# 지난 챕터 대비 오류가 약 31% 줄어든 것을 볼 수 있음
+# 물론 지금은 지난 챕터의 실습과 이번 챕터의 실습 모두 각 한 번 씩만 돌린 것이기 때문에 적절한 비교라고는 볼 수 없음
+# 만약 제대로 비교하고자 한다면, 최소한 5번 이상 같은 실험을 반복하여
+# 평균 테스트 정확도를 측정한 후, ERR을 계산해볼 수 있을 것임
 
 import pandas as pd
 from sklearn.metrics import confusion_matrix
 
-pd.DataFrame(confusion_matrix(y[-1], torch.argmax(y_hat, dim=-1)),
+print('confusion matrix = \n ',pd.DataFrame(confusion_matrix(y[-1], torch.argmax(y_hat, dim=-1)),
              index=['true_%d' % i for i in range(10)],
-             columns=['pred_%d' % i for i in range(10)])
+             columns=['pred_%d' % i for i in range(10)]))
 
-# 혼동 행렬confusion matrix을 살펴보면, 역시 이전 챕터에 비해 대각 성분의 값이 더 커지고 나머지 값들은 줄어든 것을 확인할 수 있습니다.
-# 그나마 현재 모델의 가장 큰 약점은 4를 9로 잘못 예측하는 경우가 가끔 있다는 점이라고 볼 수 있습니다.
+# confusion matrix =
+#           pred_0  pred_1  pred_2  pred_3  ...  pred_6  pred_7  pred_8  pred_9
+# true_0     971       1       0       0  ...       3       0       2       1
+# true_1       0    1128       0       1  ...       4       0       1       1
+# true_2       3       1    1008       4  ...       3       9       2       0
+# true_3       1       0       1     994  ...       0       3       3       5
+# true_4       0       0       2       0  ...       5       2       1       6
+# true_5       2       0       0      10  ...       5       3       4       1
+# true_6       2       2       1       1  ...     943       0       2       0
+# true_7       2       2       8       2  ...       0    1004       2       8
+# true_8       1       1       6       3  ...       2       2     950       6
+# true_9       3       1       0       5  ...       0       0       0     987
+# 혼동 행렬을 살펴보면, 역시 이전 챕터에 비해 대각 성분의 값이 더 커지고 나머지 값들은 줄어든 것을 확인할 수 있음
+# 그나마 현재 모델의 가장 큰 약점은 4를 9로 잘못 예측하는 경우가 가끔 있다는 점이라고 볼 수 있음
 
-# 이처럼 우리는 정규화regularization를 도입하여 모델의 오버피팅을 최대한 지연시키고 일반화 성능을 향상시킬 수 있음을 확인하였습니다.
-# 이 과정에서 정규화 계층을 간편하게 모델에 삽입하기 위하여, nn.Module을 상속받은 클래스를 통해 서브 모듈을 정의하여 nn.Sequential에 반복하여 사용하였습니다.
-# 이제 다음 챕터에서는 이제까지 배운 내용들을 총동원하여 MNIST 분류를 좀 더 제대로 구현하는 방법을 살펴보도록 하겠습니다.
+# 이처럼 우리는 정규화를 도입하여 모델의 오버피팅을 최대한 지연시키고
+# 일반화 성능을 향상시킬 수 있음을 확인하였음
+# 이 과정에서 정규화 계층을 간편하게 모델에 삽입하기 위하여, nn.Module을 상속받은 클래스를 통해
+# 서브 모듈을 정의하여 nn.Sequential에 반복하여 사용하였음
+# 이제 다음 챕터에서는 이제까지 배운 내용들을 총동원하여
+# MNIST 분류를 좀 더 제대로 구현하는 방법을 살펴보도록 하겠음
