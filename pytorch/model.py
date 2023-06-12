3925fc85272425333f592acb216ea0cad479b0a7
BangKyungHyun <bangkh21@naver.com>
2023-06-12 17:41:56 +0900

    수정

--- pytorch/model.py
+++ pytorch/model.py
@@ -1,6 +1,25 @@
+###############################################################################
+# 분류기 모델 클래스를 정의
+###############################################################################
+# 반복되는 형태를 블럭으로 만들어 크기만 다르게 한 후, 필요한 만큼 쌓을 수 있도록 구현할 것임
+# 따라서 블럭을 서브 모듈로 넣기 위해 먼저 클래스로 정의
+
 import torch
 import torch.nn as nn
 
+# 하나의 블럭은 nn.Linear 계층, nn.LeakyReLU 활성 함수, nn.BatchNorm1d 계층
+# 또는 nn.Dropout 계층 3개로 이루어져 nn.Sequential에 차례대로 선언되어 있음.
+# 눈여겨 보아야 할 점은, get_regularizer 함수를 통해
+# use_batch_norm이 True이면  nn.BatchNorm1d 계층을 넣어주고,
+#                  False이면 nn.Dropout 계층을 넣어준다는 점
+# 이렇게 선언된 nn.Sequential은 self.block에 지정되어,
+# forward 함수에서 피드포워드 되도록 간단하게 구현됨
+#
+# 이렇게 선언된 블럭을 모델은 반복해서 재활용 할 수 있음
+# 다음의 코드는 최종 모델로써, 앞서 선언된 블럭을 재활용하여 아키텍처를 구성하도록 되어 있음
+# 참고로 이 모델은 이후에 작성할 코드에서 MNIST 데이터를 28×28 이 아닌
+# 784차원의 벡터로 잘 변환했으리라 가정한 코드임.
+# 따라서 추후에 올바른 데이터를 넣어주도록 잊지말고 구현해 주어야 함
 
 class Block(nn.Module):
 
@@ -26,6 +45,10 @@ class Block(nn.Module):
             get_regularizer(use_batch_norm, output_size),
         )
 
+    # forward() 함수는 model 객체를 데이터와 함께 호출하면 자동으로 실행
+    # 예를 들어 model이란 이름의 객체를 생성 후, model(입력 데이터)와 같은 형식으로
+    # 객체를 호출하면 자동으로 forward 연산이 수행
+
     def forward(self, x):
         # |x| = (batch_size, input_size)
         y = self.block(x)
@@ -33,7 +56,15 @@ class Block(nn.Module):
 
         return y
 
-
+# nn.Sequential을 활용하여 블럭을 필요한 만큼 쌓도록 함. 
+# 여기서 클래스 선언시에 입력을 받은 hidden_sizes를 통해 필요한 블럭의 갯수와 
+# 각 블럭의 입출력 크기를 알 수 있음
+# 따라서 hidden_sizes를 활용하여 for 반복문 안에서
+# Block 클래스를 선언하여 blocks라는 리스트에 넣어줌. 
+# 이렇게 채워진 blocks를 nn.Sequential에 바로 넣어주고, 
+# 이어서 각 클래스별 로그 확률 값을 표현하기위한 nn.Linear와 nn.LogSoftmax를 넣어줌. 
+# 이후 self.layers에 선언한 nn.Sequential 객체를 넣어주어, 
+# forward 함수에서 피드포워드하도록 구현하였음을 볼 수 있음
 class ImageClassifier(nn.Module):
 
     def __init__(self,
@@ -68,4 +99,4 @@ class ImageClassifier(nn.Module):
         y = self.layers(x)
         # |y| = (batch_size, output_size)
 
-        return
+        return y
--- pytorch/trainer.py
+++ pytorch/trainer.py
@@ -12,6 +12,8 @@ class Trainer():
 
         super().__init__()
 
+    # 다음의 _batchify 함수는 매 에포크마다 SGDStochastic Gradient Descent를
+    # 수행하기 위해 shuffling 후, 미니배치를 만드는 과정입니다.
     def _batchify(self, x, y, batch_size, random_split=True):
         if random_split:
             indices = torch.randperm(x.size(0), device=x.device)
@@ -22,6 +24,18 @@ class Trainer():
         y = y.split(batch_size, dim=0)
 
         return x, y
+
+    # 이 함수는 한 epoch의 학습을 위한 for 반복문을 구현하였습니다.
+    # 함수의 시작부분에서 잊지 않고 train() 함수를 호출해서
+    # 모델을 학습 모드로 전환하는 것을 확인할 수 있습니다.
+    # 만약 이 라인이 생략된다면 이전 에포크의 validation 과정에서
+    # 추론 모드였던 모델이 그대로 학습에 활용될 것입니다.
+    # for 반복문은 작은 루프를 담당하고,
+    # 해당 반복문의 내부는 미니배치의 feed-forward와 back-propagation,
+    # 그리고 gradient descent에 의한 파라미터 업데이트가 담겨있습니다.
+    # 마지막으로 config.verbose에 따라 현재 학습 현황을 출력합니다.
+    # config는 가장 바깥의 train.py에서 사용자의 실행시 파라미터 입력에 따른 설정값이 들어있는 객체입니다.
+    # _train 함수의 가장 첫부분에 _batchify 함수를 호출하는 것을 볼 수 있습니다.
 
     def _train(self, x, y, config):
         self.model.train()
@@ -47,6 +61,10 @@ class Trainer():
 
         return total_loss / len(x)
 
+    # validation 과정에서는 random_split이 필요 없으므로 False로 넘어올 수 있음을 유의하세요.
+    # 다음 코드는 검증 과정을 위한 _validate 함수입니다.
+    # 대부분 _train 과 비슷하게 구현되어 있음을 알 수 있습니다.
+    # 다만 가장 바깥 쪽에 torch.no_grad()가 호출되어 있음을 유의하세요.
     def _validate(self, x, y, config):
         # Turn evaluation mode on.
         self.model.eval()
@@ -66,6 +84,20 @@ class Trainer():
                 total_loss += float(loss_i)
 
             return total_loss / len(x)
+
+    # train과 validation 등을 아우르는 큰 loop가 있었고, 학습과 검증 내의 작은 루프가 있었습니다.
+    # train 함수 내의 for 반복문은 큰 루프를 구현한 것입니다.
+    # 따라서 내부에는 self._train 함수와 self._validate 함수를 호출하는 것을 볼 수 있습니다.
+    # 그리고 곧이어 검증 손실 값에 따라 현재까지의 모델을 따로 저장하는 과정도 구현된 것을 확인할 수 있습니다.
+    #
+    # 현재까지의 최고 성능 모델을 best_model 변수에 저장하기 위해서는
+    # state_dict라는 함수를 사용하는 것을 볼 수 있는데요.
+    # 이 state_dict 함수는 모델의 가중치 파라미터 값들을 json 형태로 변환하여 리턴합니다.
+    # 이 json 값의 메모리를 best_model에 저장하는 것이 아닌,
+    # 값 자체를 새로 복사하여 best_model에 할당하는 것을 볼 수 있습니다.
+    # 그리고 학습이 종료되면 best_model에 저장된 가중치 파라미터 json 값을
+    # load_state_dict를 통해 self.model에 다시 로딩하는 것을 볼 수 있습니다.
+    # 이 마지막 라인을 통해서 학습 종료 후에 오버피팅이 되지 않은 가장 좋은 상태의 모델로 복원할 수 있게 됩니다.
 
     def train(self, train_data, valid_data, config):
         lowest_loss = np.inf
