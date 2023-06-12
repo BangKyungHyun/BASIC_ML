25a4f556867f513d14f72fba58fe50db886fdd4d
BangKyungHyun <72430106+BangKyungHyun@users.noreply.github.com>
2023-06-13 03:35:19 +0900

    수정

--- pytorch/predict.py
+++ pytorch/predict.py
@@ -1,3 +1,4 @@
+
 import torch
 import torch.nn
 
@@ -11,12 +12,15 @@ from utils import load_mnist
 from utils import split_data
 from utils import get_hidden_sizes
 
-# 학습을 마치면 가중치 파라미터가 담긴 파일이 torch.save 함수를 활용하여 피클 형태로 저장되어 있을 것입니다.
-# 그럼 이제 해당 모델 파일을 불러와서 추론 및 평가를 수행하는 코드를 구현해야합니다.
+# 학습을 마치면 가중치 파라미터가 담긴 파일이 torch.save 함수를 활용하여 피클 형태로
+# 저장되어 있을 것입니다.
+# 그럼 이제 해당 모델 파일을 불러와서 추론 및 평가를 수행하는 코드를 구현해야 합니다.
+
 # 보통은 train.py처럼 predict.py를 만들어서 일반 파이썬 스크립트로 짤 수도 있지만,
 # 좀 더 손쉬운 시각화를 위해서 주피터 노트북을 활용하도록 하겠습니다.
+
 # 만약 단순히 추론만 필요한 상황이라면 predict.py를 만들어서 추론 함수를 구현한 후,
-# API 서버 등에서 랩핑wrapping하는 형태로 구현할 수 있을 것입니다.
+# API 서버 등에서 wrapping하는 형태로 구현할 수 있을 것입니다.
 
 # 다음 코드는 torch.load를 활용하여 torch.save로 저장된 파일을 불러오기 위한 코드 입니다.
 
@@ -41,13 +45,16 @@ def plot(x, y_hat):
 # map_location을 통해서 내가 원하는 device로 객체를 로딩하는 것에 주목하세요.
 # 만약 map_location을 쓰지 않는다면, 자동으로 앞서 학습에 활용된
 # 디바이스로 로딩될 것입니다.
-# 같은 컴퓨터라면 크게 상관이 없을 수도 있지만, 만약 다른 컴퓨터인데 GPU가 없거나 갯수가 다르다면 문제가 될 수 있습니다.
+# 같은 컴퓨터라면 크게 상관이 없을 수도 있지만,
+
+# 만약 다른 컴퓨터인데 GPU가 없거나 갯수가 다르다면 문제가 될 수 있습니다.
 # 예를 들어 GPU 4개짜리 컴퓨터에서 3번 GPU를 활용해서 학습이 된 파일인데,
 # 추론 컴퓨터에는 0번 GPU까지만 있는 상황이라면 문제가 될 것입니다.
 #
 # 다음 코드는 추론을 직접 수행하는 코드를 test함수로 구현한 모습입니다.
 # eval() 함수를 활용하여 잊지않고 모델을 추론 모드로 바꿔주는 모습입니다.
 # 또한 torch.no_grad()를 활용하여 효율적으로 텐서 계산 연산하기 위한 모습도 확인할 수 있습니다.
+
 def test(model, x, y, to_be_shown=True):
     model.eval()
 
@@ -89,12 +96,16 @@ model = ImageClassifier(
     dropout_p=train_config.dropout_p,
 ).to(device)
 
-# load_state_dict는 json 형태의 모델 가중치가 저장된 객체를 실제 모델 객체에 로딩하는 함수입니다.
+# load_state_dict는 json 형태의 모델 가중치가 저장된 객체를 실제 모델 객체에 로딩하는
+# 함수입니다.
 # 앞서 트레이너trainer 코드를 설명할 때에도 사용된 것을 볼 수 있었는데요.
 # 중요한 점은 load_state_dict를 사용하기에 앞서,
 # ImageClassifer 객체를 먼저 선언하여 model 변수에 할당하는 것을 볼 수 있습니다.
-# 즉, 이렇게 생성된 model 객체는 임의로 초기화(random initialized)된 가중치 파라미터 값을 가지고 있을텐데요.
-# 이것을 load_state_dict 함수를 통해 기존이 학습이 완료된 가중치 파라미터 값으로 바꿔치지 하는 것으로 이해할 수 있습니다.
+# 즉, 이렇게 생성된 model 객체는 임의로 초기화(random initialized)된 가중치 파라미터 값을
+# 가지고 있을텐데요.
+# 이것을 load_state_dict 함수를 통해 기존이 학습이 완료된 가중치 파라미터 값으로
+# 바꿔치지 하는 것으로 이해할 수 있습니다.
+
 model.load_state_dict(model_dict)
 
 # 마지막에 test 함수에 전체 테스트셋을 넣어주어,
