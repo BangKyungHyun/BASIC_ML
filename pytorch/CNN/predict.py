6cbd5a9bc853385ff3a52216baaec06a18d09ffc
BangKyungHyun <72430106+BangKyungHyun@users.noreply.github.com>
2023-06-14 05:22:48 +0900

    수정

---
+++ pytorch/CNN/predict.py
@@ -0,0 +1,70 @@
+import torch
+import torch.nn
+
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+from mnist_classifier.utils import load_mnist
+from mnist_classifier.utils import get_model
+
+model_fn = "./model.pth"
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+
+def load(fn, device):
+    d = torch.load(fn, map_location=device)
+
+    return d['model'], d['config']
+
+def plot(x, y_hat):
+    for i in range(x.size(0)):
+        img = (np.array(x[i].detach().cpu(), dtype='float')).reshape(28,28)
+
+        plt.imshow(img, cmap='gray')
+        plt.show()
+        print("Predict:", float(torch.argmax(y_hat[i], dim=-1)))
+
+
+def test(model, x, y, to_be_shown=True):
+    model.eval()
+
+    with torch.no_grad():
+        y_hat = model(x)
+
+        correct_cnt = (y.squeeze() == torch.argmax(y_hat, dim=-1)).sum()
+        total_cnt = float(x.size(0))
+
+        accuracy = correct_cnt / total_cnt
+        print("Accuracy: %.4f" % accuracy)
+
+        if to_be_shown:
+            plot(x, y_hat)
+
+model_dict, train_config = load(model_fn, device)
+
+print(train_config)
+
+# Load MNIST test set.
+x, y = load_mnist(is_train=False, flatten=(train_config.model == "fc"))
+x, y = x.to(device), y.to(device)
+
+print(x.shape, y.shape)
+
+input_size = int(x.shape[-1])
+output_size = int(max(y)) + 1
+
+model = get_model(
+    input_size,
+    output_size,
+    train_config,
+    device,
+)
+
+model.load_state_dict(model_dict)
+
+test(model, x, y, to_be_shown=False)
+
+n_test = 20
+test(model, x[:n_test], y[:n_test], to_be_shown=True)
