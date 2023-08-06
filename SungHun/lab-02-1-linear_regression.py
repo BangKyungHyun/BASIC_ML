1937cff44c1c182a4db78cd4181806ed113f6633
BangKyungHyun <72430106+BangKyungHyun@users.noreply.github.com>
2023-08-06 12:20:57 +0900

    수정

--- SungHun/lab-02-1-linear_regression.py
+++ SungHun/lab-02-1-linear_regression.py
@@ -8,31 +8,28 @@ y_train = [1, 2, 3]
 W = tf.Variable(tf.random.normal([1]), name='weight')
 b = tf.Variable(tf.random.normal([1]), name='bias')
 
-# XW+b를 이용하여 만든 예측
+# XW+b를 이용하여 만든 추정값
 hypothesis = x_train * W + b
 
-# cost/loss function
+# 실제값(y_train)과 추정값(hypothesis) 사이의 차이(비용)(MSE : 평균제곱오류)
 cost = tf.reduce_mean(tf.square(hypothesis - y_train))
 
-#optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-5) # 최적값
-#train = optimizer.minimize(cost)
-
+# 경사 하강법에 의한 가중치 최적화(학습률 0.1)
 train = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(cost)
-#train = tf.train.GradientDescentOptimizer(learning_rate=1e-5).minimize(cost)
+
 # Launch the graph in a session.
 sess = tf.Session()
 
 # Initializes global variables in the graph.
 sess.run(tf.global_variables_initializer())
 
-for step in range(200000001):
+for step in range(1001):
+    # 학습 수행
     sess.run(train)
     
-    if step % 100000 == 0:
+    if step % 10 == 0:
         now         = datetime.datetime.now()
         nowDatetime = now.strftime('%Y-%m-%d %H:%M:%S')
-        #cost_val, x_train_val, W_val, b_val, y_train_val, hy_val = sess.run([cost, x_train, W, b, y_train, hypothesis])
-        cost_val, W_val, b_val,  hy_val = sess.run([cost, W, b,  hypothesis])
 
-        #print(nowDatetime,"Step:", format(step,',d'), "Cost:", cost_val, "x_train:", x_train_val, "W:", W_val, "B:", b_val, "y_train:", y_train_val, "hypothesis:", hy_val)
+        cost_val, W_val, b_val,  hy_val = sess.run([cost, W, b,  hypothesis])
         print(nowDatetime,"Step:", format(step,',d'), "Cost:", cost_val, "W:", W_val, "B:", b_val,  "hypothesis:", hy_val)
